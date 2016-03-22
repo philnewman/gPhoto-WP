4b8762b0831669f9aabf9693d4b2da9c82c63b0c
Phil Newman <phil.newman@gmail.com>
2016-03-21 17:38:39 -0700

    Renamed functions

--- includes/settings.php
+++ includes/settings.php
@@ -1,6 +1,6 @@
 <?php
 
-	function ptn_getPhotoURL(){
+	function ptn_gPhoto_WP_getPhotoURL(){
 		$PICASAWEB_USER	= get_option("pwaplusphp_picasa_username");	
 		$PICASAWEB_USER = strstr($PICASAWEB_USER,'@',true);
 		$file = 'https://picasaweb.google.com/data/feed/api/user/'.$PICASAWEB_USER;
