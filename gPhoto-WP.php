248433af79384f8350d4f31b667ce6bb13923993
Phil Newman <phil.newman@gmail.com>
2016-03-21 17:39:43 -0700

    Renamed functions

--- gPhoto-WP.php
+++ gPhoto-WP.php
@@ -15,14 +15,14 @@ error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
 // Ensure that PWA+PHP is installed
 // ----------------------------------------------------------------------------------------------------------
 
-function gPhoto_WP_activate() {
+function ptn_gPhoto_WP_activate() {
 	$PWAPLUSPHP = 'pwaplusphp/pwaplusphp.php';
 	if (!is_plugin_active($PWAPLUSPHP)){
 		deactivate_plugins(plugin_basename(__FILE__));
 		wp_die('<a href="https://wordpress.org/plugins/pwaplusphp/"> pwaplusphp is required.</a>');
 	}
 }
-register_activation_hook( __FILE__, 'gPhoto_WP_activate' );
+register_activation_hook( __FILE__, 'ptn_gPhoto_WP_activate' );
 
 
 // ----------------------------------------------------------------------------------------------------------
@@ -36,19 +36,6 @@ function ptn_gPhoto_WP_PhotoUpload_shortcode( $atts, $content = null ) {
 	ptn_uploadPhotos($albumId);
 	
 } // end shortcode
-
-// ----------------------------------------------------------------------------------------------------------
-// Create a new Google Photo Album with WP-Upload Page
-// ----------------------------------------------------------------------------------------------------------
-function ptn_gPhoto_WP_CreateAlbum_shortcode(){
-	echo "In create album shortcode";
-/*
-		b-create WP-page w/ [ptnpicasa] shortcode
-	3 - if dup 
-		a-error message
-		b-reset form
-	*/
-}
 
 // ----------------------------------------------------------------------------------------------------------
 // Includes
